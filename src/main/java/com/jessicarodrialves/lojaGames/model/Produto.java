880c2c569f5d360175cb6c437f28ee53ce99de45
Jessica <jessica.rodrialves@outlook.com>
2025-03-10 13:26:35 -0300

    criação da estrutura MVC e implementação dos metodos CRUD

---
+++ src/main/java/com/jessicarodrialves/lojaGames/model/Produto.java
@@ -0,0 +1,79 @@
+package com.jessicarodrialves.lojaGames.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_produto")
+public class Produto {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank(message = "O atributo nome é obrigatorio")
+	@Size(min = 2, max = 500, message = "O nome tem que ser maior que 2 e menor que 500")
+	private String nome;
+			
+	@NotNull(message = "O atributo preco é obrigatorio")
+	private Double preco;
+	
+	@NotNull(message = "O atributo quantidade é obrigatorio")
+	private int quantidade;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("produto")
+	
+	private Categoria categoria;
+
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+
+	public int getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+
+}
---
+++ src/main/java/com/jessicarodrialves/lojaGames/repository/CategoriaRepository.java
@@ -0,0 +1,13 @@
+package com.jessicarodrialves.lojaGames.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+import com.jessicarodrialves.lojaGames.model.Categoria;
+
+public interface CategoriaRepository extends JpaRepository<Categoria, Long>{
+
+	public List<Categoria> findAllByTipoCategoriaContainingIgnoreCase(@Param("tipoCategoria") String tipoCategoria);
+}
